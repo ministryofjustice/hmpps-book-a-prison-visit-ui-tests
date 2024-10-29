4b731a6fead5fbe8d64fa216f8cc143ccd2803b1
Vamsi-QA <vamsi.mandapati@digital.justice.gov.uk>
2024-10-29 21:12:53 +0000

    acceptance tests for VB-4268

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -4,11 +4,21 @@ import { BasePage } from './BasePage'
 export default class HomePage extends BasePage {
   private readonly prisonerName: Locator
   private readonly startButton: Locator
+  private readonly cancelBookingRadio: Locator
+  private readonly keepBookingRadio: Locator
+  private readonly confirmButton: Locator
+  private readonly confirmationMessage: Locator
+  private readonly bookingRefNUmber: Locator
 
   constructor(page: Page) {
     super(page)
     this.prisonerName = page.getByTestId('prisoner-name')
     this.startButton = page.getByTestId('start-booking')
+    this.cancelBookingRadio = page.getByRole('radio', { name: 'Yes, cancel this booking' })
+    this.keepBookingRadio = page.getByRole('radio', { name: 'No, keep this booking' })
+    this.confirmButton = page.getByTestId('confirm-button')
+    this.confirmationMessage = page.locator('[id$=main-content]')
+    this.bookingRefNUmber = page.getByTestId('booking-reference')
   }
 
   async getPrisonerName(): Promise<string> {
@@ -26,4 +36,24 @@ export default class HomePage extends BasePage {
   async setAuthCookiesInStorage(path: string): Promise<void> {
     await this.page.context().storageState({ path })
   }
+
+  async cancelBooking(): Promise<void> {
+    await this.cancelBookingRadio.click()
+  }
+
+  async clickConfirm(): Promise<void> {
+    await this.confirmButton.click()
+  }
+
+  async getConfirmationMessage(): Promise<string> {
+    return this.confirmationMessage.innerText()
+  }
+  async selectKeepThislBooking(): Promise<void> {
+    await this.keepBookingRadio.click()
+  }
+
+  async getBookingRefNumber(): Promise<string> {
+    return this.bookingRefNUmber.innerText()
+  }
+
 }
