cf3c45c119a19f63fda91a9a4d32d0e248243326
Vamsi-QA <vamsi.mandapati@digital.justice.gov.uk>
2024-12-17 13:05:13 +0000

    update tests to include start page

--- src/tests/viewBookableVisitorsDetails.spec.ts
+++ src/tests/viewBookableVisitorsDetails.spec.ts
@@ -13,7 +13,8 @@ test.describe('Verify details of visitors a booker can book for', () => {
     test.beforeEach(async ({ context, loginPage, homePage }) => {
         context.clearCookies()
         await loginPage.navigateTo('/')
-        await loginPage.checkOnPage('Create your GOV.UK One Login or sign in')
+        await loginPage.checkOnPage('Visit someone in prison')
+        await loginPage.clickStartNowButton()
         await loginPage.goToSignInPage()
         await loginPage.signInWith(UserType.USER_NAME)
     })
